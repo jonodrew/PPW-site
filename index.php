baf625527981c2e36ea8390c9b59926c29b50e04
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-20 12:08:00 +0100

    Inserted images and updated index file.
    Some images may need resizing.

--- index.php
+++ index.php
@@ -15,30 +15,24 @@
 
     <div class="about-us">
 	     <div class="container">
+         <div class="row">
+           <h2 class="text-center"><em>Proudly supported by</em></h2>
+         </div>
+         <div class="row">
+           <div class="clearfix visible-xs-block"></div>
+           <div class="col-xs-6 col-sm-4"><img src="/images/biffaward.jpg" class="img-responsive"></div>
+           <div class="col-xs-6 col-sm-4"><img src="/images/twells.jpg" class="img-responsive"></div>
+           <div class="col-xs-6 col-sm-4"><img src="/images/waitrose.png" class="img-responsive"></div>
+           <!-- Optional: clear the XS cols if their content doesn't match in height -->
+           </div>
+           <div class="row">
+             <div class="clearfix visible-xs-block"></div>
+             <div class="col-xs-6 col-sm-4"><img src="/images/botanix.jpg" class="img-responsive"></div>
+             <div class="col-xs-6 col-sm-4"><img src="/images/pwcouncil.png" class="img-responsive"></div>
+             <div class="col-xs-6 col-sm-4"><img src="/images/mack.gif" class="img-responsive"></div>
+           </div> 
 
-	  <div class="row">
-	      <div class="col-md-4">
-			<h3>Students Without Borders</h3>
-			<p>We love to travel! Every year we organise trips to political hubs to
-        expand our minds and get stuck into other cultures. We've been to New York, D.C., Paris, Brussels, and London.</p>
-
-	      </div>
-		  <div class="col-md-4">
-			<h3>Best Society</h3>
-			<p>We've won Best Society on Campus for three years running, and that's
-        because we work hard to create amazing events that are open to everyone.
-        If you've got an idea for an event, don't hesitate to get in touch - through
-        Facebook, Twitter, or by emailing us:
-        <a href="mailto:abdn.pir.society@googlemail.com">abdn.pir.society@googlemail.com</a></p>
-		  </div>
-		  <div class="col-md-4">
-			<h3>Non partisan. Totally political.</h3>
-			<p>We're not affiliated to any political party - we bring together a whole
-      range of political philosophies, from right to left. So whichever way you
-      lean, we'd love to get to know you.</p>
-		  </div>
-	    </div>
-	  </div>
-	</div>
+</div>
+</div>
   </body>
 </html>
