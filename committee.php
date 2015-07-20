165fcdc9952cf1ace34b3513498bcbc634089531
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-20 15:02:37 +0100

    Added first officer line in html

--- committee.php
+++ committee.php
@@ -12,5 +12,16 @@ include('head.php');
 </div>
 </div>
 
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-3 col-xs-5">
+  <img src="/images/butterfly.png" class="img-thumbnail">
+</div>
+<div class="col-md-9 col-xs-11">
+  <h2>Kay Groves</h2><br>
+  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+</div>
+</div>
+
 </body>
 </html>
