9c9c32668f4b8cafc740cb0811f5baae3bdbb01a
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 00:43:36 +0100

    add: some text

--- achievements.php
+++ achievements.php
@@ -12,6 +12,7 @@ include('head.php');
   </div>
 </div>
 
-
+Since 2009, we've been on a mission to improve the play areas of Paddock Wood.<br>
+And we've been seriously successful.
 
 <?php include("footer.php"); ?>
