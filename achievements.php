8c914a50f5ec407e7fd44d01c7cb95b88790cc8e
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 00:32:00 +0100

    framework for "achievements" page

---
+++ achievements.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = 'Achievements';
+include('head.php');
+?>
+
+
+<div id="header"></div>
+
+<div class="banner">
+  <div class="container-fluid">
+    <h1>Achievements</h1>
+  </div>
+</div>
+
+
+
+<?php include("footer.php"); ?>
