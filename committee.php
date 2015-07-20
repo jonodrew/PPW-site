412e4d76ad989f17be0e6beee9abd2b13d4a15a5
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-20 21:10:14 +0100

    added names of all members apart from councillors

--- committee.php
+++ committee.php
@@ -20,7 +20,7 @@ include('head.php');
         <img src="/images/butterfly.png" class="img-thumbnail">
       </div>
       <div class="col-md-8 col-xs-11">
-        <h2>Kay Groves</h2><br>
+        <h2>Kay Groves: Chair</h2><br>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
     </div>
@@ -38,7 +38,7 @@ include('head.php');
         <img src="/images/butterfly.png" class="img-thumbnail">
       </div>
       <div class="col-md-8 col-xs-11">
-        <h2>Secretary</h2><br>
+        <h2>Wendy Kent: Secretary</h2><br>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
     </div>
@@ -63,19 +63,19 @@ include('head.php');
   </div>
   <div class="row">
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Jane Kerr<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Marie Marriott<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Eileen Wilson<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Alison Robinson<br></h2>
       <h3><em>Title</em></h3>
     </div>
   </div>
@@ -96,19 +96,19 @@ include('head.php');
   </div>
   <div class="row">
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Ros Tucker<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>David Sargasson<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Town Council<br></h2>
       <h3><em>Title</em></h3>
     </div>
     <div class="col-md-3 col-xs-4">
-      <h2>Name Surname<br></h2>
+      <h2>Town Council<br></h2>
       <h3><em>Title</em></h3>
     </div>
   </div>
