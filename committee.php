a8006515b985c4ebf9b041f872124e51200fb593
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-20 19:59:35 +0100

    added bottom row, fixed spacing

--- committee.php
+++ committee.php
@@ -12,16 +12,110 @@ include('head.php');
 </div>
 </div>
 
-<div class="container-fluid">
-<div class="row">
-<div class="col-md-3 col-xs-5">
-  <img src="/images/butterfly.png" class="img-thumbnail">
+<div class="officers">
+
+    <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3 col-xs-5">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+      </div>
+      <div class="col-md-8 col-xs-11">
+        <h2>Kay Groves</h2><br>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-offset-1 col-md-8 col-xs-11">
+        <h2>Claire Holly: Treasurer</h2><br>
+        <p>I have been the Treasurer for Play Paddock Wood since we started in November 2009. I have over 20 years of finance experience, I am a licensed Member in Practice of the Association of Accounting Technicians, and I run my own practice from my home in Paddock Wood. I have lived here for over 7 years with my husband Mark and our son Jack, who attends Paddock Wood Primary. I enjoy working with such a great group of people to make a real difference in our community, and I am proud of everything we have achieved.</p>
+      </div>
+      <div class="col-md-3 col-xs-5">
+        <img src="/images/butterfly.png" class="img-circle">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 col-xs-5">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+      </div>
+      <div class="col-md-8 col-xs-11">
+        <h2>Secretary</h2><br>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="col-md-9 col-xs-11">
-  <h2>Kay Groves</h2><br>
-  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+<div class="members">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3 col-xs-4">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+      </div>
+      <div class="col-md-3 col-xs-4">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+      </div>
+      <div class="col-md-3 col-xs-4">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+    </div>
+      <div class="col-md-3 col-xs-4">
+        <img src="/images/butterfly.png" class="img-thumbnail">
+      </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+  </div>
+  <div class="bottom-row">
+  <div class="row">
+    <div class="col-md-3 col-xs-4">
+      <img src="/images/butterfly.png" class="img-thumbnail">
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <img src="/images/butterfly.png" class="img-thumbnail">
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <img src="/images/butterfly.png" class="img-thumbnail">
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <img src="/images/butterfly.png" class="img-thumbnail">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+    <div class="col-md-3 col-xs-4">
+      <h2>Name Surname<br></h2>
+      <h3><em>Title</em></h3>
+    </div>
+  </div>
+  </div>
 </div>
 </div>
+
 
 </body>
 </html>
