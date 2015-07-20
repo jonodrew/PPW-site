328c9cf132c057a1a032e4fa2595c8644190c20c
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 00:53:49 +0100

    added: framework

---
+++ play-area.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = 'Play Areas';
+include('head.php');
+?>
+
+<div id="header"></div>
+
+<div class="banner">
+  <div class="container-fluid">
+    <h1>Play Areas</h1>
+  </div>
+</div>
+
+<h1>Listof play areas</h1>
+
+<?php include("footer.php"); ?>
