0e5619d0382f37668d91caa310509dac715a9590
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 00:45:00 +0100

    added: framework

---
+++ carnival.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = 'Carnival';
+include('head.php');
+?>
+
+
+<div id="header"></div>
+
+<div class="banner">
+  <div class="container-fluid">
+    <h1>Carnival</h1>
+  </div>
+</div>
