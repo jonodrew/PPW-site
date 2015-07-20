01efc3b7d3b7b610bb53b9cc635f7d93e2086f7d
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 00:58:34 +0100

    add: text

--- achievements.php
+++ achievements.php
@@ -12,7 +12,13 @@ include('head.php');
   </div>
 </div>
 
-Since 2009, we've been on a mission to improve the play areas of Paddock Wood.<br>
-And we've been seriously successful.
+<h2>Since 2009, we've been on a mission to improve the play areas of Paddock Wood.<br>
+And we've been seriously successful.<br>
+
+We reached out to our local community, asked them what they want, and then we did it.<br>
+
+It's not rocket science.<br>
+
+Just as well, really.</h2>
 
 <?php include("footer.php"); ?>
