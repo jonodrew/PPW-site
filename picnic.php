c1435e67428b6589e2e5a31e9ac1a5fdcc876e3a
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 01:08:41 +0100

    added: picnic framework

---
+++ picnic.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = 'Picnic in the Park';
+include('head.php');
+?>
+
+<div id="header"></div>
+
+<div class="banner">
+  <div class="container-fluid">
+    <h1>Picnic in the Park</h1>
+  </div>
+</div>
+
+<h1>Every year we get together and enjoy the amazing green spaces in Paddock Wood.</h1>
+
+<?php include("footer.php"); ?>
