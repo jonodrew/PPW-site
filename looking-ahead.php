49123c959d1f5b42a3d01c50be2254f48035b6a0
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-21 01:05:10 +0100

    added: framework

---
+++ looking-ahead.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = 'Looking Ahead';
+include('head.php');
+?>
+
+
+<div id="header"></div>
+
+<div class="banner">
+  <div class="container-fluid">
+    <h1>Looking Ahead</h1>
+  </div>
+</div>
+
+
+<?php include("footer.php"); ?>
