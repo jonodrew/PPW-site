d2d47379eed20e0330897201b9ea1711c54ae56f
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-22 23:08:42 +0100

    mobile-optimised committee

--- committee.php
+++ committee.php
@@ -12,107 +12,80 @@ include('head.php');
 </div>
 </div>
 
+<div id="mobile-screen-committee">
 <div id="officers">
 
     <div class="container-fluid">
     <div class="row">
-      <div class="col-md-3 col-xs-5">
+      <div class="col-md-4">
         <img src="/images/butterfly.png" class="img-circle">
-      </div>
-      <div class="col-md-8 col-xs-11">
         <h2>Kay Groves: Chair</h2><br>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
-    </div>
-    <div class="row">
-      <div class="col-md-offset-1 col-md-8 col-xs-11">
+      <div class="col-md-4">
+        <img src="/images/Claire-Holly.jpg" class="img-circle">
         <h2>Claire Holly: Treasurer</h2><br>
         <p>I have been the Treasurer for Play Paddock Wood since we started in November 2009. I have over 20 years of finance experience, I am a licensed Member in Practice of the Association of Accounting Technicians, and I run my own practice from my home in Paddock Wood. I have lived here for over 7 years with my husband Mark and our son Jack, who attends Paddock Wood Primary. I enjoy working with such a great group of people to make a real difference in our community, and I am proud of everything we have achieved.</p>
       </div>
-      <div class="col-md-3 col-xs-5">
-        <img src="/images/Claire-Holly.jpg" class="img-circle">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-3 col-xs-5">
+      <div class="col-md-4">
         <img src="/images/butterfly.png" class="img-circle">
-      </div>
-      <div class="col-md-8 col-xs-11">
         <h2>Wendy Kent: Secretary</h2><br>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
-    </div>
   </div>
 </div>
 
 <div class="members">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-3 col-xs-4">
+      <div class="col-md-3">
         <img src="/images/butterfly.png" class="img-circle">
+        <h2>Jane Kerr<br></h2>
+        <h3><em>Title</em></h3>
       </div>
-      <div class="col-md-3 col-xs-4">
+      <div class="col-md-3">
         <img src="/images/marie.jpg" class="img-rounded">
+        <h2>Marie Marriott<br></h2>
+        <h3><em>Title</em></h3>
       </div>
-      <div class="col-md-3 col-xs-4">
+      <div class="col-md-3">
         <img src="/images/butterfly.png" class="img-circle">
-    </div>
-      <div class="col-md-3 col-xs-4">
+        <h2>Eileen Wilson<br></h2>
+        <h3><em>Title</em></h3>
+      </div>
+      <div class="col-md-3">
         <img src="/images/butterfly.png" class="img-circle">
+        <h2>Alison Robinson<br></h2>
+        <h3><em>Title</em></h3>
       </div>
   </div>
-  <div class="row">
-    <div class="col-md-3 col-xs-4">
-      <h2>Jane Kerr<br></h2>
-      <h3><em>Title</em></h3>
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <h2>Marie Marriott<br></h2>
-      <h3><em>Title</em></h3>
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <h2>Eileen Wilson<br></h2>
-      <h3><em>Title</em></h3>
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <h2>Alison Robinson<br></h2>
-      <h3><em>Title</em></h3>
-    </div>
-  </div>
+
   <div class="bottom-row">
   <div class="row">
-    <div class="col-md-3 col-xs-4">
+    <div class="col-md-3">
       <img src="/images/butterfly.png" class="img-circle">
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <img src="/images/butterfly.png" class="img-circle">
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <img src="/images/butterfly.png" class="img-circle">
-    </div>
-    <div class="col-md-3 col-xs-4">
-      <img src="/images/butterfly.png" class="img-circle">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-3 col-xs-4">
       <h2>Ros Tucker<br></h2>
       <h3><em>Title</em></h3>
     </div>
-    <div class="col-md-3 col-xs-4">
+    <div class="col-md-3">
+      <img src="/images/butterfly.png" class="img-circle">
       <h2>David Sargisson<br></h2>
       <h3><em>Title</em></h3>
     </div>
-    <div class="col-md-3 col-xs-4">
+    <div class="col-md-3">
+      <img src="/images/butterfly.png" class="img-circle">
       <h2>Town Council<br></h2>
       <h3><em>Title</em></h3>
     </div>
-    <div class="col-md-3 col-xs-4">
+    <div class="col-md-3">
+      <img src="/images/butterfly.png" class="img-circle">
       <h2>Town Council<br></h2>
       <h3><em>Title</em></h3>
     </div>
   </div>
+
   </div>
+</div>
 </div>
 </div>
 
