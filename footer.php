18f9b0c3eda9333c32bfc2601c4bc058ada66127
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-07-23 15:17:39 +0100

    fixed

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 <div class="footer">
   <footer>
     <div class="container-fluid">
-      <div class="nav">
         <ul>
           <ul class="pull-right">
             <li><a href="https://www.facebook.com/groups/216112215097879/?fref=ts"><img src="/images/Facebook.png" class="img-rounded"></a></li>
@@ -10,7 +9,6 @@
             <li><a href="#">&copy;Play Paddock Wood 2015</a></li>
           </div>
         </ul>
-      </div>
     </footer>
   </div>
 
--- index.php
+++ index.php
@@ -33,20 +33,15 @@
          <div class="row">
            <h2 class="text-center"><em>Proudly supported by</em></h2>
          </div>
-         <div class="row">
-           <div class="clearfix visible-xs-block"></div>
-           <div class="col-xs-6 col-sm-4"><img src="/images/biffaward.jpg" class="img-responsive"></div>
-           <div class="col-xs-6 col-sm-4"><img src="/images/twells.jpg" class="img-responsive"></div>
-           <div class="col-xs-6 col-sm-4"><img src="/images/waitrose.png" class="img-responsive"></div>
-           <!-- Optional: clear the XS cols if their content doesn't match in height -->
-         </div>
-           <div class="row">
-             <div class="clearfix visible-xs-block"></div>
-             <div class="col-xs-6 col-sm-4"><img src="/images/botanix.jpg" class="img-responsive"></div>
-             <div class="col-xs-6 col-sm-4"><img src="/images/pwcouncil.png" class="img-responsive"></div>
-             <div class="col-xs-6 col-sm-4"><img src="/images/mack.png" class="img-responsive"></div>
 
-           </div>
+
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/biffaward.jpg" class="img-responsive"></div>
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/twells.jpg" class="img-responsive"></div>
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/waitrose.png" class="img-responsive"></div>
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/botanix.jpg" class="img-responsive"></div>
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/mack.png" class="img-responsive"></div>
+          <div class="col-xs-6 col-md-4 col-lg-4"><img src="/images/pwcouncil.png" class="img-responsive"></div>
+
          </div>
     </div>
 
